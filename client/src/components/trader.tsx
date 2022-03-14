5043f6da32dde0af4c4017be4489a80f23e8b0f8
Drew Rodrigues <thesimpledev@gmail.com>
2022-03-14 14:16:06 -0600

    Show whether or not the market is open

--- client/src/components/trader.tsx
+++ client/src/components/trader.tsx
@@ -6,6 +6,7 @@ import {
   Tooltip
 } from 'chart.js'
 import classNames from 'classnames'
+import moment from 'moment'
 import React, { useEffect, useState } from 'react'
 import { Bar } from 'react-chartjs-2'
 import { LiveBar } from '../../../common'
@@ -14,13 +15,17 @@ import { dollarFormatter } from './strategyResponse'
 
 ChartJS.register(CategoryScale, LinearScale, BarElement, Tooltip)
 
+const MARKET_OPEN = moment('9:30am', 'h:mma')
+const MARKET_CLOSE = moment('4:00pm', 'h:mma')
+
 export function Trader() {
   const { socketStatus, socket } = useSocket()
   const [liveData, setLiveData] = useState<{
     bars: LiveBar[]
     recentBars: LiveBar[]
     currentPrice: number
-  }>({ bars: [], currentPrice: 0, recentBars: [] })
+    shouldBuy: boolean | undefined
+  }>({ bars: [], currentPrice: 0, recentBars: [], shouldBuy: undefined })
 
   useEffect(() => {
     if (socketStatus === 'connected') {
@@ -32,6 +37,10 @@ export function Trader() {
       })
     }
   }, [socketStatus])
+
+  const marketTime = new Date().toLocaleString('en-US', {
+    timeZone: 'America/New_York'
+  })
 
   const data = {
     labels: liveData.bars.map((bar) => bar.time),
@@ -54,6 +63,7 @@ export function Trader() {
     ]
   }
 
+  const isMarketOpen = moment(marketTime).isBetween(MARKET_OPEN, MARKET_CLOSE)
   console.log({ data: liveData })
 
   return (
@@ -72,8 +82,26 @@ export function Trader() {
         </h3>
       </header>
 
+      <h4
+        className={classNames('mb-[20px]', {
+          'text-green-400': isMarketOpen,
+          'text-red-400': !isMarketOpen
+        })}
+      >
+        {isMarketOpen ? '📈 Market Open ' : '❌ Market Closed '}
+        <span className="font-bold">{moment(marketTime).format('h:mm A')}</span>
+      </h4>
+
       <div className="mb-[20px] bg-[#333] border-stone-700 p-[20px] rounded-[5px]">
-        <h2 className="font-bold text-white mb-[10px]">Recent (30 mins)</h2>
+        <h2 className="font-bold text-white mb-[10px]">Positions</h2>
+
+        {liveData.shouldBuy !== undefined && (
+          <p>shouldBuy?: {liveData.shouldBuy ? '✅' : '❌'}</p>
+        )}
+      </div>
+
+      <h2 className="font-bold text-white mb-[10px]">Recent (30m)</h2>
+      <div className="mb-[20px] bg-[#333] border-stone-700 p-[20px] rounded-[5px]">
         <Bar
           data={recentData}
           options={{
