49faccb4f36bc857b0a812552ca2252de77a28a8
Drew Rodrigues <thesimpledev@gmail.com>
2022-03-15 13:54:10 -0600

    Display open positions & orders on client

--- client/src/components/trader.tsx
+++ client/src/components/trader.tsx
@@ -9,7 +9,7 @@ import classNames from 'classnames'
 import moment from 'moment'
 import React, { useEffect, useState } from 'react'
 import { Bar } from 'react-chartjs-2'
-import { LiveBar } from '../../../common'
+import { LiveBar, OpenOrder, OpenPosition } from '../../../common'
 import { useSocket } from '../hooks/useSocket'
 import { dollarFormatter } from './strategyResponse'
 
@@ -19,14 +19,24 @@ const MARKET_OPEN = moment('9:30am', 'h:mma')
 const MARKET_CLOSE = moment('4:00pm', 'h:mma')
 
 export function Trader() {
-  const { socketStatus, socket } = useSocket()
   const [liveData, setLiveData] = useState<{
+    // TODO: pull to common to use for socket and client
     bars: LiveBar[]
     recentBars: LiveBar[]
     currentPrice: number
     shouldBuy: boolean | undefined
-  }>({ bars: [], currentPrice: 0, recentBars: [], shouldBuy: undefined })
+    openOrders: OpenOrder[]
+    openPositions: OpenPosition[]
+  }>({
+    bars: [],
+    currentPrice: 0,
+    recentBars: [],
+    shouldBuy: undefined,
+    openOrders: [],
+    openPositions: []
+  })
 
+  const { socketStatus, socket } = useSocket()
   useEffect(() => {
     if (socketStatus === 'connected') {
       console.log('Listening for barUpdate')
@@ -47,7 +57,7 @@ export function Trader() {
     datasets: [
       {
         data: Object.values(liveData.bars).map((liveBar) => liveBar.close),
-        backgroundColor: 'red'
+        backgroundColor: 'rgb(248 113 113)'
       }
     ]
   }
@@ -58,7 +68,7 @@ export function Trader() {
         data: Object.values(liveData.recentBars).map(
           (liveBar) => liveBar.close
         ),
-        backgroundColor: 'red'
+        backgroundColor: 'rgb(248 113 113)'
       }
     ]
   }
@@ -92,12 +102,66 @@ export function Trader() {
         <span className="font-bold">{moment(marketTime).format('h:mm A')}</span>
       </h4>
 
-      <div className="mb-[20px] bg-[#333] shadow-md border border-stone-700 p-[20px] rounded-[5px]">
-        <h2 className="font-bold text-white mb-[10px]">Positions</h2>
+      <div className="mb-[20px] bg-[#333] shadow-md border border-stone-700 p-[20px] rounded-[5px] text-white">
+        <h2 className="font-bold text-white mb-[10px]">Open Positions</h2>
 
-        {liveData.shouldBuy !== undefined && (
-          <p>shouldBuy?: {liveData.shouldBuy ? '✅' : '❌'}</p>
-        )}
+        {/* // TODO: color based upon current position */}
+        {liveData.openPositions.map((position) => (
+          <div
+            className="border border-green-700 px-[10px] py-[5px] rounded-[5px] bg-green-400 flex justify-between shadow-md"
+            key={`openPosition-${position.contract.conId}`}
+          >
+            <p>
+              <span className="font-bold">{`${position.contract.symbol} `}</span>
+              {position.lotSize} @
+              {position.averageCost &&
+                ` ${dollarFormatter.format(position.averageCost)}`}
+            </p>
+
+            {position.averageCost && (
+              <p>
+                {dollarFormatter.format(
+                  position.averageCost * position.lotSize
+                )}
+              </p>
+            )}
+          </div>
+        ))}
+      </div>
+
+      <div className="mb-[20px] bg-[#333] shadow-md border border-stone-700 p-[20px] rounded-[5px] text-white">
+        <h2 className="font-bold text-white mb-[10px]">Open Orders</h2>
+
+        {liveData.openOrders.map((openOrder) => (
+          <div
+            className={classNames(
+              'border px-[10px] py-[5px] rounded-[5px]  flex justify-between shadow-md',
+              {
+                'bg-red-400 border-red-700': openOrder.order.action === 'SELL',
+                'bg-green-400 border-green-700':
+                  openOrder.order.action === 'BUY'
+              }
+            )}
+            key={`openOrder-${openOrder.contract.conId}`}
+          >
+            <p>
+              {`${openOrder.order.action} `}
+              {openOrder.order.totalQuantity}
+              <span className="font-bold">{` ${openOrder.contract.symbol} `}</span>
+              @
+              {openOrder.order.lmtPrice &&
+                ` ${dollarFormatter.format(openOrder.order.lmtPrice)}`}
+            </p>
+
+            {openOrder.order.lmtPrice && openOrder.order.totalQuantity && (
+              <p>
+                {dollarFormatter.format(
+                  openOrder.order.lmtPrice * openOrder.order.totalQuantity
+                )}
+              </p>
+            )}
+          </div>
+        ))}
       </div>
 
       <h2 className="font-bold text-white mb-[10px]">Recent (30m)</h2>
