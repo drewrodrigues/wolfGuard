83cbd0cd619bf7593382d90d3c308da0a2a8fd63
Drew Rodrigues <thesimpledev@gmail.com>
2022-03-17 15:00:40 -0600

    Show historical data in chart w/ query param from client

--- client/src/views/historyView.tsx
+++ client/src/views/historyView.tsx
@@ -1,6 +1,59 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import {
+  BarElement,
+  CategoryScale,
+  Chart as ChartJS,
+  LinearScale,
+  Tooltip
+} from 'chart.js'
+import { useRequest } from '../hooks/request'
+import { Bar } from 'react-chartjs-2'
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Tooltip)
 
 // TODO: get the last day of data and display it
 export function HistoryView() {
-  return <>History View</>
+  // ! hard type this with Bar[]
+  const historicalBarsRequest = useRequest<{ bars: any[] }>()
+
+  useEffect(() => {
+    historicalBarsRequest.call(
+      `/bars/MSFT?startDate='2020-12-28'&endDate='2020-12-29'`
+    )
+  }, [])
+
+  let data
+
+  if (historicalBarsRequest.requestStatus === 'success') {
+    data = {
+      labels: historicalBarsRequest.data.bars.map((bar) => bar.time),
+      datasets: [
+        {
+          data: Object.values(historicalBarsRequest.data.bars).map(
+            (liveBar) => liveBar.close
+          ),
+          backgroundColor: 'rgb(248 113 113)'
+        }
+      ]
+    }
+  }
+
+  return (
+    <>
+      {data && (
+        <Bar
+          data={data}
+          options={{
+            scales: {
+              x: { display: false },
+              y: {
+                beginAtZero: false,
+                grid: { drawBorder: true, color: '#777' }
+              }
+            }
+          }}
+        />
+      )}
+    </>
+  )
 }
--- server/src/controllers/barsController.ts
+++ server/src/controllers/barsController.ts
@@ -3,10 +3,22 @@ import { db } from '../access/db'
 const router = express.Router()
 
 router.get('/:symbol', async (req, res) => {
+  const { startDate = '2000-01-01', endDate = '2100-01-01' } = req.query
+  console.log({ query: { startDate, endDate } })
+
   try {
-    const bars = await db.bar.findMany({ where: { symbol: req.params.symbol } })
+    const bars = await db.bar.findMany({
+      where: {
+        symbol: req.params.symbol,
+        time: {
+          gte: new Date(startDate as string),
+          lte: new Date(endDate as string)
+        }
+      }
+    })
     res.send({ bars })
   } catch (e) {
+    console.error(e)
     res.status(500).send({ error: e })
   }
 })
